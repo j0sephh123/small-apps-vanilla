38c18355c85317e8b3aae9b580bff53822cc4f7a
j0sephh123 <iossif123gg@gmail.com>
2022-12-29 23:32:08 +0200

    add todos

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const decrement = document.getElementById("decrement");
 const counter = document.getElementById("counter");
 const increment = document.getElementById("increment");
-let counterValue = 0;
+const reset = document.getElementById("reset");
+const INITIAL_COUNTER_VALUE = 0;
+let counterValue = INITIAL_COUNTER_VALUE;
 
-function update() {
-  counter.innerHTML = counterValue;
-  // TODO check if reset should be disabled
-  // TODO check if correct class to counterValue is applied
+function disableCounterIfInitialValue(isInitialValue) {
+  isInitialValue
+    ? reset.setAttribute("disabled", true)
+    : reset.removeAttribute("disabled");
 }
 
-// TODO create a common function to handle both inc and dec
-increment.addEventListener('click', () => {
-  counterValue += 1;
+function applyCorrectClass(isInitialValue) {
+  if (isInitialValue) {
+    counter.removeAttribute("class");
+  } else {
+    counter.className = counterValue > 0 ? "success" : "failure";
+  }
+}
+
+function update() {
+  const isInitialValue = counterValue === INITIAL_COUNTER_VALUE;
+  disableCounterIfInitialValue(isInitialValue);
+  applyCorrectClass(isInitialValue);
+  counter.innerHTML = counterValue;
+}
+
+function act(shouldIncrement) {
+  if (shouldIncrement) {
+    counterValue += 1;
+  } else {
+    counterValue -= 1;
+  }
   update();
-})
-decrement.addEventListener('click', () => {
-  counterValue -= 1;
+}
+
+function resetCounter() {
+  counterValue = INITIAL_COUNTER_VALUE;
   update();
-})
+}
+
+increment.addEventListener("click", () => act(true));
+decrement.addEventListener("click", () => act(false));
+reset.addEventListener('click', resetCounter)
 
 update();
